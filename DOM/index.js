20987b3ef03cc3a1ecae4a476126310925547d13
kameshwar sah <56642875+kameshwar-web@users.noreply.github.com>
2023-10-05 10:03:36 +0530

    All project

---
+++ DOM/index.js
@@ -0,0 +1,14 @@
+// alert("hi");
+// var h = document.querySelector('h1');
+// h.textContent = "hello java script";
+// h.style.color = "red";
+
+
+// var xx=document.querySelector("#main");
+// xx.textContent = "hello";
+
+// xx.style.color ="yellow";
+var a= function(){
+    var a=10;
+    console.log(a);
+}
---
+++ javascript2/style.js
@@ -0,0 +1,66 @@
+// alert("hi");
+// Array
+// () -> functio
+// {} -> scop / blank object
+// [] -> array
+
+// var arr = ["hello", "javascript","hello", "javascript"];
+// console.log(arr);
+// console.log(arr.length);
+
+// var len=arr.length -1;
+// console.log(len);
+
+// var arr = [1,2,3,4,5];
+// console.log(arr);
+// arr.push(6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+// For each loop
+
+// var user = ['x', 'y', 'z'];
+
+// user.forEach(function(value){
+//     //console.log(value);
+//     console.log(value + " hello");
+// })
+
+
+// ER5 and ES6
+// 1. function statement
+function add(){
+var a=20;
+var b=30;
+var sum=a+b;
+console.log(sum);
+}
+add();
+// 2. function expression 
+var add = function (){
+
+}
+
+//3 function annoums
+function (){
+
+}
+
+// 4. fat Arrow Function
+var add () =>{
+
+}
+
+// 5 Arrow function with one para
+var add(value) =>{
+
+}
+// 6. fat arroe function with implicit return
+
+var add = value => 0
+//or
+var add = value => {
+    return 0;
+}
